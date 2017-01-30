94e24e9276cb7a27e2beb30b67b4579a8ca53bdf
skabobo <skabobo@hotmail.com>
2017-01-30 10:13:50 +0100

    småfix

--- Abbas-projekt/connect.php
+++ Abbas-projekt/connect.php
@@ -1,7 +1,8 @@
 <?php 
 
 // Create connection
-$db = mysqli_connect("localhost", "root", "", "abbashodroj");
+//$db = mysqli_connect("localhost", "root", "", "abbashodroj");
+$db = mysqli_connect("abbashodroj-219305.mysql.binero.se", "219305_im16622", "hodroj13", "219305-abbashodroj");
 
 mysqli_query($db, "SET NAMES utf8");
 
